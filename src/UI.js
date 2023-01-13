38619dfbdf385d332e992f7ee7f88603c602c78f
Trygve-F <trygvefinley@gmail.com>
2023-01-13 00:59:17 -0800

    added logic to get input value of 'New Project' PopUp, add it to array, and render the updated array when a new project is added

--- src/UI.js
+++ src/UI.js
@@ -1,19 +1,24 @@
 import Task from './task'
 import Project from './project'
 
+const projectArray = []
+
 export default class UI {
+
 
 //LOAD HOMEPAGE
 static loadHomepage = () => {
     UI.initNewProjectButtons();
     UI.initNewTaskButtons();
-    }
+    UI.initDefaultProjects();
+    UI.renderProjects();
+    };
 
 // INIT TASK BUTTONS
 static initNewTaskButtons = () => {
     const newTaskButton = document.getElementById('new-task-button');
-    newTaskButton.addEventListener('click', UI.checkNewTaskPopUp)
-}
+    newTaskButton.addEventListener('click', UI.checkNewTaskPopUp);
+};
 
 static checkNewTaskPopUp = () => {
     
@@ -23,7 +28,7 @@ static checkNewTaskPopUp = () => {
         UI.newTaskPopUp();
         }
     return;
-}
+};
 
 static newTaskPopUp = () => {
     const newTaskNav = document.getElementById('new-task-nav');
@@ -34,10 +39,9 @@ static newTaskPopUp = () => {
     const newTaskAddButton = document.createElement('button');
     newTaskAddButton.innerHTML = ' + ';
     newTaskAddButton.addEventListener('click', () => {
-        UI.createTask();
+        UI.addTaskToArray();
         UI.closeNewTaskPopUp();
-
-})
+});
 
 
     const newTaskCancel = document.createElement('button');
@@ -48,24 +52,24 @@ static newTaskPopUp = () => {
     newTaskNav.appendChild(newTaskPopUp);
     newTaskNav.appendChild(newTaskAddButton);
     newTaskNav.appendChild(newTaskCancel);
-}
+};
 
 
 static closeNewTaskPopUp = () => {
     const newTaskNav = document.getElementById('new-task-nav');
     while (newTaskNav.childNodes.length > 2) {
     newTaskNav.removeChild(newTaskNav.lastChild)
-}
-}
+    };
+};
 
-static checkDuplicateTasks = () => {
-    //
-    createTask();
-}
 
-static createTask = () => {
-    alert('wow');
-}
+static addTaskToArray = () => {
+    //find active project
+    //add task to active projects array
+    const taskArray = []
+    
+
+};
 
 // INIT PROJECT BUTTONS
     
@@ -73,51 +77,89 @@ static initNewProjectButtons = () => {
     const newProjectButton = document.getElementById('new-project-button');
 
     newProjectButton.addEventListener('click', UI.checkNewProjectPopUp);
-    }
+    }; 
 
 static newProjectPopUp = () => {
     const newProjectNav = document.getElementById('new-project-nav');
 
-    const newProjectPopUp = document.createElement('input');
+    const newProjectInput = document.createElement('input');
+    newProjectInput.id = 'new-project-input'
     const newProjectAddButton = document.createElement('button');
-    newProjectAddButton.id = 'newProjectAddButton';
+    newProjectAddButton.id = 'new-project-add-button';
     newProjectAddButton.innerHTML = '+';
     newProjectAddButton.addEventListener('click', () => {
-        UI.createProject();
+        UI.addProjectToArray();
+        UI.renderProjects();
         UI.closeProjectButtonPopUp();
-    })
+    });
 
     const newProjectCancel = document.createElement('button');
     newProjectCancel.innerHTML = 'X';
     newProjectCancel.addEventListener('click', UI.closeProjectButtonPopUp);
 
-    newProjectNav.appendChild(newProjectPopUp);
+    newProjectNav.appendChild(newProjectInput);
     newProjectNav.appendChild(newProjectAddButton);
     newProjectNav.appendChild(newProjectCancel);
-}
+};
 
 static closeProjectButtonPopUp = () => {
-const newProjectNav = document.getElementById('new-project-nav');
-while (newProjectNav.childNodes.length > 2) {
-    newProjectNav.removeChild(newProjectNav.lastChild)
-}
-}
+    const newProjectNav = document.getElementById('new-project-nav');
+    while (newProjectNav.childNodes.length > 2) {
+        newProjectNav.removeChild(newProjectNav.lastChild);
+    };
+};
 
 static checkNewProjectPopUp = () => {
-const checkPopUp = document.getElementById('new-project-nav');
+    const checkPopUp = document.getElementById('new-project-nav');
 
-if (checkPopUp.childElementCount === 1) {
-    UI.newProjectPopUp();
+    if (checkPopUp.childElementCount === 1) {
+        UI.newProjectPopUp();
+        };
+    return;
+};
+
+//PROJECTS CONTAINER
+
+static initDefaultProjects = () => {
+    projectArray.push(new Project('Today'));
+    projectArray.push(new Project('This Week'));
     }
-return;
+
+
+
+static getProjectInputValue = () => {
+    const value = document.getElementById('new-project-input').value;
+    return value;
 }
 
-//PAGE FUNCTIONS
-
-static createProject = () => {
-    const newProject = new Project('dingus',);
-    alert(newProject.getName())
+static addProjectToArray = () => {
+    const value = UI.getProjectInputValue();
+    projectArray.push(new Project(value));
 }
 
-    
+static renderProjects = () => {
+    const container = document.getElementById('new-project-container')
+    container.innerHTML = '';
+    projectArray.forEach((e) => {
+        const temp = document.createElement('div');
+        temp.innerHTML = e.getName();
+        container.appendChild(temp)
+        })
 }
+
+ 
+};
+
+
+
+
+
+/* static createDefaultProjects = () => {
+    const projectArray = []
+    projectArray.push(new Project('Today'));
+    projectArray.push(new Project('This Week'));
+
+    projectArray.forEach((e) => {
+        alert(e.getName())
+    })
+} */
